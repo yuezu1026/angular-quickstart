2d11bf32b5002fc8cf3249e77c6f285c858652ac
yuezu1026 <39002818@qq.com>
2019-07-24 18:08:51 +0800

    First2 Commit

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  template: '<h1>我的第一个 Angular 应用wangyasdkdsfdsfozu</h1>'
+  template: '<h1>我的第一个 Angular 应用wangyasdkdsfdsfozudfsdfdsdsfdsf</h1>'
 })
 export class AppComponent { }
